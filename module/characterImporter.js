eaac412cb39d25b23448b3b86a84b04075a4d0a7
TJ <Cyr-@users.noreply.github.com>
2021-04-13 21:39:53 -0500

    Merge clean up

--- module/characterImporter.js
+++ module/characterImporter.js
@@ -44,27 +44,6 @@ export default class CharacterImporter {
       }
     };
 
-    const skills = {
-      acr: { value: sourceCharacter.attribs.find((e) => e.name == "acrobatics_type").current },
-      ani: { value: sourceCharacter.attribs.find((e) => e.name == "animal_handling_type").current },
-      ath: { value: sourceCharacter.attribs.find((e) => e.name == "athletics_type").current },
-      dec: { value: sourceCharacter.attribs.find((e) => e.name == "deception_type").current },
-      ins: { value: sourceCharacter.attribs.find((e) => e.name == "insight_type").current },
-      inv: { value: sourceCharacter.attribs.find((e) => e.name == "investigation_type").current },
-      itm: { value: sourceCharacter.attribs.find((e) => e.name == "intimidation_type").current },
-      lor: { value: sourceCharacter.attribs.find((e) => e.name == "lore_type").current },
-      med: { value: sourceCharacter.attribs.find((e) => e.name == "medicine_type").current },
-      nat: { value: sourceCharacter.attribs.find((e) => e.name == "nature_type").current },
-      per: { value: sourceCharacter.attribs.find((e) => e.name == "persuasion_type").current },
-      pil: { value: sourceCharacter.attribs.find((e) => e.name == "piloting_type").current },
-      prc: { value: sourceCharacter.attribs.find((e) => e.name == "perception_type").current },
-      prf: { value: sourceCharacter.attribs.find((e) => e.name == "performance_type").current },
-      slt: { value: sourceCharacter.attribs.find((e) => e.name == "sleight_of_hand_type").current },
-      ste: { value: sourceCharacter.attribs.find((e) => e.name == "stealth_type").current },
-      sur: { value: sourceCharacter.attribs.find((e) => e.name == "survival_type").current },
-      tec: { value: sourceCharacter.attribs.find((e) => e.name == "technology_type").current }
-    };
-
     /* ----------------------------------------------------------------- */
     /*  character.data.skills.<skill_name>.value is all that matters
     /*  values can be 0, 0.5, 1 or 2
@@ -140,8 +119,7 @@ export default class CharacterImporter {
         skills: skills,
         attributes: {
           hp: hp
-        },
-        skills: skills
+        }
       }
     };
 

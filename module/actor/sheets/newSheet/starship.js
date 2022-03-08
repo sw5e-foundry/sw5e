bc0562ba1cfc1a805705d90b722489c114a113b7
Ikaguia <Ikaguia@users.noreply.github.com>
2022-03-08 02:02:47 -0300

    Removing deployments on starships
    Only deployed position remaining is pilot

--- module/actor/sheets/newSheet/starship.js
+++ module/actor/sheets/newSheet/starship.js
@@ -109,7 +109,7 @@ export default class ActorSheet5eStarship extends ActorSheet5e {
                 // Item toggle state
                 this._prepareItemToggleState(item);
 
-                if (item.type === "starshipaction" || item.type === "deploymentfeature") arr[0].push(item);
+                if (["starshipaction", "deploymentfeature"].includes(item.type)) arr[0].push(item);
                 else if (["feat", "starship", "starshipfeature"].includes(item.type)) arr[1].push(item);
                 else if (item.data.equipped) arr[2].push(item);
                 else arr[3].push(item);
@@ -128,35 +128,10 @@ export default class ActorSheet5eStarship extends ActorSheet5e {
         // Organize Starship Actions
         for (const action of actions) {
             if (action.type === "starshipaction") {
-                ssActions[action.data.deployment].items.push(action);    
+                ssActions[action.data.deployment].items.push(action);
             }
-            // Deployment names don't match up with what Starship Actions call them. Need to call out "Coordinator" explicitly and push to "coord".
-            // For each item, find the item in the deployed character's features and add it.
-            else {
-                if (action.data.deployment.value.toLowerCase() === "coordinator") { 
-                    if(data.data.attributes.deployment.coord.uuid !== null) {
-                        let uuid = data.data.attributes.deployment.coord.uuid.split('.')[1];
-                        let actor = game.actors.get(uuid);
-                        let item = actor.items.find(i => i.name == action.name);
-                        if (item) ssActions.coord.items.push(action);
-                    }
-                }
-                else if (action.data.deployment.value.toLowerCase() === "gunner") {
-                    for (let gunner of data.data.attributes.deployment.gunner.items) {
-                        let uuid = gunner.uuid.split('.')[1];
-                        let actor =  game.actors.get(gunner.uuid.split('.')[1]);
-                        let item = actor.items.find(i => i.name == action.name);
-                            if (item) ssActions.gunner.items.push(action);
-                    }
-                }
-                else {
-                    if(data.data.attributes.deployment[action.data.deployment.value.toLowerCase()].uuid !== null) { 
-                        let uuid = data.data.attributes.deployment[action.data.deployment.value.toLowerCase()].uuid.split('.')[1];
-                        let actor = game.actors.get(uuid);
-                        let item = actor.items.find(i => i.name == action.name);
-                        if (item) ssActions[action.data.deployment.value.toLowerCase()].items.push(action);
-                    }
-                }
+            else if (action.type === "deploymentfeature") {
+                (ssActions[action.data.deployment.value] || ssActions.crew).items.push(action);
             }
         }
 
--- module/crew-deployment.js
+++ module/crew-deployment.js
@@ -1,69 +1,29 @@
+import {fromUuidSynchronous} from "./helpers.js";
+
 export default function CrewDeployment(doc, ship) {
 
+    //Get the list of deployed party members.
+    const deployedParty = new Set();
+    if (doc.crew.items !== null)
+        for (let c of doc.crew.items)
+            deployedParty.add(c.uuid);
+    if (doc.pilot.uuid !== null)
+        deployedParty.add(doc.pilot.uuid);
 
-//Get the list of deployed party members.
+    // Get the list of deployment features for each deployed actor.
+    // Add the deployment features to the Starship actor.
 
-const deployedParty = [];
-if (doc.coord.uuid != null) {
-    deployedParty.push(doc.coord.uuid);
-}
-
-if (doc.uuid != null) {
-    deployedParty.push(doc.gunner.uuid);
-}
-
-if (doc.gunner.items != null) {
-    for (let g of doc.gunner.items) {
-        deployedParty.push(g.uuid);
-    }
-}
-
-if (doc.mechanic.uuid != null) {
-    deployedParty.push(doc.mechanic.uuid);
-}
-
-if (doc.operator.uuid != null) {
-    deployedParty.push(doc.operator.uuid);
-}
-
-if (doc.pilot.uuid != null) {
-    deployedParty.push(doc.pilot.uuid);
-}
-
-if (doc.technician.uuid != null) {
-    deployedParty.push(doc.technician.uuid);
-}   
-
-//Filter for only unique actors.
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
-let deployments = deployedParty.filter(onlyUnique);
-
-// Get the list of deployment features for each deployed actor.
-// Add the deployment features to the Starship actor.
-
-async function addItems() {
     const values = Array.from(ship.data.items.values());
-    for (let p of deployments) {
-        if (typeof p !== "undefined") {        
-            let actorid = p.split('.');
-            let actor = game.actors.get(actorid[1]);
-            let itemsFromActor = actor.data.items.filter(t => t.data.type == "deploymentfeature");
-            for(let i of itemsFromActor) {
-                if (values.some(n => n.data.name === i.data.name)) {
-                    continue;
-                }
-                else {
-                    let newItem = [];
-                    newItem.push(i);
-                    await ship.createEmbeddedDocuments("Item", newItem.map(m => m.toObject()));
-                }
+    for (const uuid of deployedParty.values()) {
+        const actor = fromUuidSynchronous(uuid);
+        const itemsFromActor = actor.data.items.filter(t => t.data.type == "deploymentfeature");
+        for(const item of itemsFromActor) {
+            if (values.some(n => n.data.name === item.data.name)) continue;
+            else {
+                let newItem = [];
+                newItem.push(item);
+                await ship.createEmbeddedDocuments("Item", newItem.map(m => m.toObject()));
             }
         }
     }
-}
-    addItems();        
-
 }

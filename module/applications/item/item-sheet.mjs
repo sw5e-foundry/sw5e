3b46b2091d330175ef87fc733c1e761ff1943e68
Ikaguia <Ikaguia@users.noreply.github.com>
2023-03-02 14:41:12 -0300

    Consume Ammo on Save Weapons

--- module/applications/item/item-sheet.mjs
+++ module/applications/item/item-sheet.mjs
@@ -431,7 +431,7 @@ export default class ItemSheet5e extends ItemSheet {
                     props.push(
                         ...Object.entries(this.item.system.properties)
                             .filter((e) => ![false, undefined, null, 0].includes(e[1]))
-                            .map((e) => game.i18n.format(CONFIG.SW5E.weaponProperties[e[0]]?.full, {value: e[1]}))
+                            .map((e) => game.i18n.format(CONFIG.SW5E.weaponProperties[e[0]]?.full ?? "?", {value: e[1]}))
                     );
                 break;
 
